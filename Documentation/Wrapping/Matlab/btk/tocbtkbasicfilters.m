d5b1211836d47bed2a13a24fb9fa1b7b69be067d
arnaud.barre <arnaud.barre@93be6e1e-6fcc-11de-baec-69240fdc299a>
2009-08-09 19:40:20 +0000

    [FIX] Filename for the Matlab function btkGetGroundReactionWrenches was mistyped.
    git-svn-id: https://b-tk.googlecode.com/svn/BTK/trunk@171 93be6e1e-6fcc-11de-baec-69240fdc299a

--- Documentation/Wrapping/Matlab/btk/tocbtkbasicfilters.m
+++ Documentation/Wrapping/Matlab/btk/tocbtkbasicfilters.m
@@ -3,7 +3,7 @@ function tocbtkbasicfilters
 % ----------------
 %
 % Acquisition
-%   <a href="matlab:help btkGetGroundReactionForces">btkGetGroundReactionForces</a>   - Compute ground reaction wrenches (positions, forces and moments)
+%   <a href="matlab:help btkGetGroundReactionWrenches">btkGetGroundReactionWrenches</a>   - Compute ground reaction wrenches (positions, forces and moments)
 %                                  between the ground and the subject from force platform's data.
 %
 % <a href="matlab:help btk">Biomechanical Toolkit TOC</a>
