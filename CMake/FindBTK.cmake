88f618e645d40d5468519f818f0265e5ccdcdd63
arnaud.barre <arnaud.barre@93be6e1e-6fcc-11de-baec-69240fdc299a>
2010-07-20 20:56:40 +0000

    [UPD] FindBTK.cmake ready to be used for external project (using the build tree or the install tree) !
    git-svn-id: https://b-tk.googlecode.com/svn/BTK/trunk@410 93be6e1e-6fcc-11de-baec-69240fdc299a

--- CMake/FindBTK.cmake
+++ CMake/FindBTK.cmake
@@ -3,17 +3,11 @@
 # found, BTK_FOUND is set to false.
 #  BTK_FOUND         - Set to true when BTK is found.
 #  BTK_USE_FILE      - CMake file to use BTK.
-#  BTK_MAJOR_VERSION - The BTK major version number.
-#  BTK_MINOR_VERSION - The BTK minor version number 
-#                       (odd non-release).
-#  BTK_BUILD_VERSION - The BTK patch level 
-#                       (meaningless for odd minor).
+#  BTK_VERSION_MAJOR - The BTK major version number.
+#  BTK_VERSION_MINOR - The BTK minor version number.
+#  BTK_VERSION_PATCH - The BTK patch level.
 #  BTK_INCLUDE_DIRS  - Include directories for BTK
 #  BTK_LIBRARY_DIRS  - Link directories for BTK libraries
-#  BTK_KITS          - List of BTK kits, in CAPS 
-#                      (COMMON,IO,) etc.
-#  BTK_LANGUAGES     - List of wrapped languages, in CAPS
-#                      (MATLAB) etc.
 # The following cache entries must be set by the user to locate BTK:
 #  BTK_DIR  - The directory containing BTKConfig.cmake.  
 #             This is either the root of the build tree,
@@ -22,7 +16,7 @@
 #
 
 # Construct consitent error messages for use below.
-SET(BTK_DIR_DESCRIPTION "directory containing BTKConfig.cmake.  This is either the root of the build tree, or PREFIX/lib/btk for an installation.")
+SET(BTK_DIR_DESCRIPTION "directory containing BTKConfig.cmake.  This is either the root of the build tree, or PREFIX/share/btk for an installation.")
 SET(BTK_DIR_MESSAGE "BTK not found.  Set the BTK_DIR cmake cache entry to the ${BTK_DIR_DESCRIPTION}")
 
 # Search only if the location is not already known.
@@ -38,18 +32,15 @@ IF(NOT BTK_DIR)
   # Construct a set of paths relative to the system search path.
   SET(BTK_DIR_SEARCH "")
   FOREACH(dir ${BTK_DIR_SEARCH2})
-    SET(BTK_DIR_SEARCH ${BTK_DIR_SEARCH}
-      ${dir}/../lib/btk
-      )
+    SET(BTK_DIR_SEARCH ${BTK_DIR_SEARCH} ${dir}/../share/CMake)
   ENDFOREACH(dir)
+  
+  MESSAGE(STATUS "${BTK_DIR_SEARCH}")
 
   #
   # Look for an installation or build tree.
   #
-  FIND_PATH(BTK_DIR UseBTK.cmake
-    # Support legacy cache files.
-    ${BTK_DIR_SEARCH_LEGACY}
-
+  FIND_PATH(BTK_DIR BTKConfig.cmake
     # Look for an environment variable BTK_DIR.
     $ENV{BTK_DIR}
 
@@ -57,8 +48,12 @@ IF(NOT BTK_DIR)
     ${BTK_DIR_SEARCH}
 
     # Look in standard UNIX install locations.
-    /usr/local/lib/btk
-    /usr/lib/btk
+    /usr/local/share
+    /usr/lib/share
+    
+    # Look in standard Windows install locations.
+    C:\\Program Files\\BTK-debug\\share\\btk-0.1\\CMake
+    C:\\Program Files (x86)\\BTK
 
     # Read from the CMakeSetup registry entries.  It is likely that
     # BTK will have been recently built.
@@ -81,30 +76,14 @@ ENDIF(NOT BTK_DIR)
 # If BTK was found, load the configuration file to get the rest of the
 # settings.
 IF(BTK_DIR)
-  # Make sure the BTKConfig.cmake file exists in the directory provided.
-  IF(EXISTS ${BTK_DIR}/BTKConfig.cmake)
-
-    # We found BTK.  Load the settings.
-    SET(BTK_FOUND 1)
-    INCLUDE(${BTK_DIR}/BTKConfig.cmake)
-
-  ELSE(EXISTS ${BTK_DIR}/BTKConfig.cmake)
-    IF(EXISTS ${BTK_DIR}/UseBTK.cmake)
-      # We found BTK 4.0 (UseBTK.cmake exists, but not BTKConfig.cmake).
-      SET(BTK_FOUND 1)
-      # Load settings for BTK 4.0.
-      INCLUDE(UseBTKConfig40)
-    ELSE(EXISTS ${BTK_DIR}/UseBTK.cmake)
-      # We did not find BTK.
-      SET(BTK_FOUND 0)
-    ENDIF(EXISTS ${BTK_DIR}/UseBTK.cmake)
-  ENDIF(EXISTS ${BTK_DIR}/BTKConfig.cmake)
+  # BTK found: loading the settings.
+  SET(BTK_FOUND 1)
+  INCLUDE(${BTK_DIR}/BTKConfig.cmake)
 ELSE(BTK_DIR)
   # We did not find BTK.
   SET(BTK_FOUND 0)
 ENDIF(BTK_DIR)
 
-#-----------------------------------------------------------------------------
 IF(NOT BTK_FOUND)
   # BTK not found, explain to the user how to specify its location.
   IF(BTK_FIND_REQUIRED)
@@ -114,4 +93,4 @@ IF(NOT BTK_FOUND)
       MESSAGE(STATUS ${BTK_DIR_MESSAGE})
     ENDIF(NOT BTK_FIND_QUIETLY)
   ENDIF(BTK_FIND_REQUIRED)
-ENDIF(BTK_FOUND)
+ENDIF(NOT BTK_FOUND)
