75eb29e096172a7e3e75c11c902b4a7137aaa911
arnaud.barre <arnaud.barre@93be6e1e-6fcc-11de-baec-69240fdc299a>
2010-09-16 06:32:01 +0000

    [ENH] New Matlab documentation (tutorial, FAQ, function's description) generated by Doxygen. Will be used as the online documentation for the support of BTK in Matlab.
    git-svn-id: https://b-tk.googlecode.com/svn/BTK/trunk@450 93be6e1e-6fcc-11de-baec-69240fdc299a

---
+++ Documentation/Wrapping/Matlab/Web/finalize.cmake
@@ -0,0 +1,32 @@
+# Functions
+FILE(GLOB files "${BTK_BINARY_DIR}/Documentation/Wrapping/Matlab/classbtk*.html")
+FOREACH(f ${files})
+  FILE(READ "${f}" IN)
+  STRING(REPLACE " Class Reference</h1>" "<h1>" OUT "${IN}")
+  STRING(REPLACE "<table class=\"memberdecls\">
+</table>
+<hr/><a name=\"_details\"></a><h2>Description</h2>" "" OUT "${OUT}")
+  FILE(WRITE "${f}" "${OUT}")
+ENDFOREACH(f)
+
+# Groups
+FILE(GLOB files "${BTK_BINARY_DIR}/Documentation/Wrapping/Matlab/group__*.html")
+FOREACH(f ${files})
+  FILE(READ "${f}" IN)
+  STRING(REPLACE "Classes" "Functions" OUT "${IN}")
+  STRING(REPLACE "<hr/><a name=\"_details\"></a><h2>Description</h2>" "" OUT "${OUT}")
+  STRING(REPLACE "class &nbsp;" "&nbsp;" OUT "${OUT}")
+  FILE(WRITE "${f}" "${OUT}")
+ENDFOREACH(f)
+
+# Class List -> Function List
+FILE(READ "${BTK_BINARY_DIR}/Documentation/Wrapping/Matlab/annotated.html" IN)
+STRING(REPLACE "<h1>Class List</h1>" "<h1>Function List</h1>" OUT "${IN}")
+STRING(REPLACE "Here are the classes, structs, unions and interfaces with brief descriptions:" "" OUT "${OUT}")
+FILE(WRITE "${BTK_BINARY_DIR}/Documentation/Wrapping/Matlab/annotated.html" "${OUT}")
+
+# Class Index -> Function Index
+FILE(READ "${BTK_BINARY_DIR}/Documentation/Wrapping/Matlab/classes.html" IN)
+STRING(REPLACE "<h1>Class Index</h1>" "<h1>Function Index</h1>" OUT "${IN}")
+FILE(WRITE "${BTK_BINARY_DIR}/Documentation/Wrapping/Matlab/classes.html" "${OUT}")
+
