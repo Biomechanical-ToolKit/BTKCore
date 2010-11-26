e5bae6ba89823f48dc9eda872f7f2130c33346cb
arnaud.barre <arnaud.barre@93be6e1e-6fcc-11de-baec-69240fdc299a>
2010-11-26 11:17:59 +0000

    [FIX] Revision r491 was also needed with GCC.
    git-svn-id: https://b-tk.googlecode.com/svn/BTK/trunk@514 93be6e1e-6fcc-11de-baec-69240fdc299a

--- Code/IO/btkC3DFileIO.cpp
+++ Code/IO/btkC3DFileIO.cpp
@@ -542,11 +542,7 @@ namespace btk
                 eventsSubject[incEvt],
                 eventsDescription[incEvt],
                 eventsId[incEvt]);
-#if defined(_MSC_VER)
             evt->SetFrame(static_cast<int>(std::floor((evt->GetTime() * pointFrameRate) + 1.5)));
-#else
-            evt->SetFrame(static_cast<int>(evt->GetTime() * pointFrameRate) + 1);
-#endif
             events->InsertItem(evt);
           }
         }
