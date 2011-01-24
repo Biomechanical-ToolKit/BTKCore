41cfd3e23faf60071fb5e7657e77f41145719bbe
arnaud.barre <arnaud.barre@93be6e1e-6fcc-11de-baec-69240fdc299a>
2011-01-24 06:05:58 +0000

    [UPD] Mokka; Help introduces the possibility to drag and drop files in the 3D view from a file browser. [UPD] Matlab: btkGetAnalogsResolution renamed btkGetAnalogResolution as indicated in the documentation. Function btkGetAnalogsResolution still exists but display a warning as deprecated. [UPD] Matlab: btkSetAnalogsResolution renamed btkSetAnalogResolution as indicated in the documentation. Function btkSetAnalogsResolution still exists but display a warning as deprecated.
    git-svn-id: https://b-tk.googlecode.com/svn/BTK/trunk@561 93be6e1e-6fcc-11de-baec-69240fdc299a

---
+++ Documentation/Wrapping/Matlab/btk/btkGetAnalogsResolution.m
@@ -0,0 +1,12 @@
+function ar = btkGetAnalogsResolution(h)
+%BTKGETANALOGSRESOLUTION Returns the ADC resolution.
+% 
+%  AR = BTKGETANALOGSRESOLUTION(H) returns the resolution of the analog to
+%  digital converter. The acquisition is represented by the handle H.  
+%  This handle is obtained by the use of a btk* function.
+
+%  Author: A. Barré
+%  Copyright 2009-2011 Biomechanical ToolKit (BTK).
+
+warning('This function is deprecated and will be removed in a next release. Use btkGetAnalogResolution instead.');
+return btkGetAnalogResolution(h);
---
+++ Documentation/Wrapping/Matlab/btk/btkSetAnalogsResolution.m
@@ -0,0 +1,13 @@
+function btkSetAnalogsResolution(h, res)
+%BTKSETANALOGSRESOLUTION Set the ADC resolution.
+% 
+%  BTKSETANALOGRESOLUTION(H, res) sets the resolution of the analog to
+%  digital converter. The value must be 8, 12, 14 or 16. The acquisition 
+%  is represented by the handle H. This handle is obtained by the use of 
+%  a btk* function.
+
+%  Author: A. Barré
+%  Copyright 2009-2011 Biomechanical ToolKit (BTK).
+
+warning('This function is deprecated and will be removed in a next release. Use btkGetAnalogResolution instead.');
+btkSetAnalogResolution(h,res);
