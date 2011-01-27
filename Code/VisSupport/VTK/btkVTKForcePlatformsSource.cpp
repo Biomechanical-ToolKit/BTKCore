61257cb2d57c31263bc933a764531e109c5d5e60
arnaud.barre <arnaud.barre@93be6e1e-6fcc-11de-baec-69240fdc299a>
2011-01-27 07:00:49 +0000

    [FIX] btk::VTKForcePlatformsSource is called two times inside Mokka each time you load an acquisition (MTimes problem?). [FIX] Force platform number is not displayed when the ground is the not the XY plane (scale error). [FIX] Force platform number orientation error when the ground is the not the XY plane. [FIX] VTK message error when there is only one force platform (Input port 0 of algorithm vtkAppendPolyData has 0 connections...)
    git-svn-id: https://b-tk.googlecode.com/svn/BTK/trunk@565 93be6e1e-6fcc-11de-baec-69240fdc299a

--- Code/VisSupport/VTK/btkVTKForcePlatformsSource.cpp
+++ Code/VisSupport/VTK/btkVTKForcePlatformsSource.cpp
@@ -131,6 +131,9 @@ namespace btk
     VTKDataObjectAdapter* inObject = VTKDataObjectAdapter::SafeDownCast(inInfo->Get(vtkDataObject::DATA_OBJECT()));
     if (!inObject)
       return 0;
+    if (inObject->GetMTime() < this->GetMTime())
+      return 0;
+
     ForcePlatformCollection::Pointer input = static_pointer_cast<ForcePlatformCollection>(inObject->GetBTKDataObject());
     vtkInformation* outInfo1 = outputVector->GetInformationObject(0);    
     vtkPolyData* output1 = vtkPolyData::SafeDownCast(outInfo1->Get(vtkDataObject::DATA_OBJECT()));
@@ -159,8 +162,8 @@ namespace btk
     vtkTransformPolyDataFilter* indexScale = vtkTransformPolyDataFilter::New();
     indexScale->SetInputConnection(indexGenerator->GetOutputPort());
     indexScale->SetTransform(transform);
-    vtkTransformPolyDataFilter* indexTranslate = vtkTransformPolyDataFilter::New();
-    indexTranslate->SetTransform(transform);
+    vtkTransformPolyDataFilter* indexTransform = vtkTransformPolyDataFilter::New();
+    indexTransform->SetTransform(transform);
     int idx = 1;
     for (ForcePlatformCollection::ConstIterator it = input->Begin() ; it != input->End() ; ++it)
     {
@@ -244,34 +247,59 @@ namespace btk
       axes->Delete();
       // Output #2: Index
       if (input->GetItemNumber() > 1)
-      {  
+      {
         indexGenerator->SetText(btk::ToString(idx++).c_str());
         indexGenerator->Update();
         vtkBoundingBox bbForcePlate(forcePlatform->GetBounds());
         vtkBoundingBox bbIndex(indexGenerator->GetOutput()->GetBounds());
-        double scale = std::min(bbForcePlate.GetLength(0) / bbIndex.GetLength(0), bbForcePlate.GetLength(1) / bbIndex.GetLength(1)) / 2.0;
+        // Scale
+        double bbLengths[2][3], maxLengths[2][3], minTwoLengths[2][2];
+        bbForcePlate.GetLengths(bbLengths[0]); bbIndex.GetLengths(bbLengths[1]);
+        for (int bbIdx = 0 ; bbIdx < 2 ; ++bbIdx)
+        {
+          maxLengths[bbIdx][0] = std::max(bbLengths[bbIdx][0], bbLengths[bbIdx][1]);
+          maxLengths[bbIdx][1] = std::max(bbLengths[bbIdx][1], bbLengths[bbIdx][2]);
+          maxLengths[bbIdx][2] = std::max(bbLengths[bbIdx][0], bbLengths[bbIdx][2]);
+          minTwoLengths[bbIdx][0] = std::min(maxLengths[bbIdx][0], maxLengths[bbIdx][1]);
+          minTwoLengths[bbIdx][1] = std::min(maxLengths[bbIdx][1], maxLengths[bbIdx][2]);
+        }
+        double scale = std::min(minTwoLengths[0][0] / minTwoLengths[1][0], minTwoLengths[0][1] / minTwoLengths[1][1]) / 2.0;
         transform->Identity();
         transform->Scale(scale, scale, scale);
         indexScale->Update();
-        bbIndex.SetBounds(indexScale->GetOutput()->GetBounds());
-        double bbIndexCenter[3]; bbIndex.GetCenter(bbIndexCenter);
-        double bbForcePlateCenter[3]; bbForcePlate.GetCenter(bbForcePlateCenter);
+        // Rotation
         vtkPolyData* index = vtkPolyData::New();
         index->DeepCopy(indexScale->GetOutput());
-        indexTranslate->SetInput(index);
+        indexTransform->SetInput(index);
         pose->Identity();
+        R.col(2) *= -1.0; // Reorient Z axis as for force platforms it is always defined as negative.
+        if (R.col(1).sum() <= 0)
+          R.col(1) *= -1.0; // Force the Y vector to be in the same direction than the global frame.
+        R.col(0) = R.col(1).cross(R.col(2));
         for (int i = 0 ; i < 3 ; ++i)
         {
-          //for (int j = 0 ; j < 3 ; ++j)
-          //  pose->SetElement(i,j,R(i,j));
-          pose->SetElement(i, 3, (bbForcePlateCenter[i] - bbIndexCenter[i]) * this->m_Scale);
+          for (int j = 0 ; j < 3 ; ++j)
+            pose->SetElement(i,j,R(i,j));
         }
-        
         transform->SetMatrix(pose);
-        indexTranslate->Update();
+        indexTransform->Update();
         index->Delete();
         index = vtkPolyData::New();
-        index->DeepCopy(indexTranslate->GetOutput());
+        index->DeepCopy(indexTransform->GetOutput());
+        // Translation
+        bbIndex.SetBounds(indexTransform->GetOutput()->GetBounds());
+        double bbIndexCenter[3]; bbIndex.GetCenter(bbIndexCenter);
+        double bbForcePlateCenter[3]; bbForcePlate.GetCenter(bbForcePlateCenter);
+        indexTransform->SetInput(index);
+        pose->Identity();
+        for (int i = 0 ; i < 3 ; ++i)
+          pose->SetElement(i, 3, (bbForcePlateCenter[i] - bbIndexCenter[i]) * this->m_Scale);
+        transform->SetMatrix(pose);
+        indexTransform->Update();
+        index->Delete();
+        index = vtkPolyData::New();
+        index->DeepCopy(indexTransform->GetOutput());
+        // Append
         append3->AddInput(index);
         index->Delete();
       }
@@ -282,7 +310,7 @@ namespace btk
     pose->Delete();
     axesColors->Delete();
     indexScale->Delete();
-    indexTranslate->Delete();
+    indexTransform->Delete();
 
     if (input->GetItemNumber() != 0)
     {
@@ -292,9 +320,14 @@ namespace btk
       append2->Update();
       output2->ShallowCopy(append2->GetOutput());
       append2->Delete();
-      append3->Update();
-      output3->ShallowCopy(append3->GetOutput());
-      append3->Delete();
+      if (input->GetItemNumber() > 1)
+      {
+        append3->Update();
+        output3->ShallowCopy(append3->GetOutput());
+        append3->Delete();
+      }
+      else
+        output3->Initialize();
     }
     else
     {
