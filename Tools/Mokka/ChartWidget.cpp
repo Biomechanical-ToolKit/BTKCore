b2d450727654c17e82143e347b35d396a3100d47
arnaud.barre <arnaud.barre@93be6e1e-6fcc-11de-baec-69240fdc299a>
2011-09-13 22:13:28 +0000

    [FIX] Mokka: Missing header under Windows XP due to the merging. The function ::Sleep requires to include the header windows.h.
    git-svn-id: https://b-tk.googlecode.com/svn/BTK/trunk@779 93be6e1e-6fcc-11de-baec-69240fdc299a

--- Tools/Mokka/ChartWidget.cpp
+++ Tools/Mokka/ChartWidget.cpp
@@ -59,6 +59,12 @@
 #include <QTreeWidget>
 #include <QToolTip>
 
+#ifdef Q_OS_WIN
+  #ifndef WIN32_LEAN_AND_MEAN
+    #define WIN32_LEAN_AND_MEAN
+  #endif
+  #include <windows.h>
+#endif
 
 class PointChartData : public AbstractChartData
 {
