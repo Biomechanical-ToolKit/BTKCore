abe75ce18ed8c67eecaed3cabe8386a1ff07accd
arnaud.barre <arnaud.barre@93be6e1e-6fcc-11de-baec-69240fdc299a>
2011-11-09 06:16:39 +0000

    [FIX] Force platform items in the model panel are not displayed if there is not 3D view visible.
    git-svn-id: https://b-tk.googlecode.com/svn/BTK/trunk@854 93be6e1e-6fcc-11de-baec-69240fdc299a

--- Tools/Mokka/MultiViewWidget.cpp
+++ Tools/Mokka/MultiViewWidget.cpp
@@ -454,7 +454,9 @@ void MultiViewWidget::load()
     for (int j = 1 ; j < this->mp_Acquisition->analogSamplePerPointFrame() ; ++j)
       this->mp_AnalogChartFrames->SetValue(inc + j, val + j * sub);
   }
-  
+  // Force the update for the generation of the force platforms and their forces (In case there is no 3D view, they are not updated).
+  forcePlaforms->Update();
+  GRFs->Update();
   // Active the content of each view
   for (QList<AbstractView*>::const_iterator it = this->views().begin() ; it != this->views().end() ; ++it)
     static_cast<CompositeView*>(*it)->show(true);
