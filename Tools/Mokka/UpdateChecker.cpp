f12eb10ed5fb1f4559968ef096941788fd0a2687
arnaud.barre <arnaud.barre@93be6e1e-6fcc-11de-baec-69240fdc299a>
2012-06-19 20:38:26 +0000

    [FIX] Mokka: The method used to check if an update exists failed to compare the release number "0.5.1" with "0.6a1". The proposed method is now more robust.
    git-svn-id: https://b-tk.googlecode.com/svn/BTK/trunk@1146 93be6e1e-6fcc-11de-baec-69240fdc299a

--- Tools/Mokka/UpdateChecker.cpp
+++ Tools/Mokka/UpdateChecker.cpp
@@ -252,47 +252,87 @@ void UpdateParser::parseReply(QNetworkReply* reply)
   reply->manager()->deleteLater();
 };
 
-bool UpdateParser::isGreaterRelease(const QStringList& max, const QStringList& rel) const
+int UpdateParser::compareRelease(const QStringList& ver1, const QStringList& ver2) const
 {
-  for (int i = 0 ; i < 3 ; ++i)
+  // Inspired by http://stackoverflow.com/questions/198431/how-do-you-compare-two-version-strings-in-java
+  // More robust method than the previous one, but still some problems with the content of the suffix (e.g. 0.1a2 > 0.1a10...)
+  int i = 0;
+  for (i = 0 ; i < ver1.count() ; ++i)
   {
-    if (!this->isGreaterRelease(max.value(i), rel.value(i)))
-      return false;
-  }
-  return true;
-};
-
-bool UpdateParser::isGreaterRelease(const QString& max, const QString& rel) const
-{
-  int n = qMax(max.count(), rel.count());
-  for (int i = 0 ; i < n ; ++i)
-  {
-    QChar m, r;
-    if (i >= max.count())
-      m = QChar::fromAscii(0x00);
-    else
-      m = max[i];
-    if (i >= rel.count())
-      r = QChar::fromAscii(0x00);
-    else
-      r = rel[i];
+    if (i >= ver2.count())
+    {
+      for (int j = i ; j < ver1.count() ; ++j)
+      {
+        QString suffix1;
+        int number1 = this->extractReleaseNumber(ver1[j], suffix1);
+        if ((number1 != 0) || (suffix1.length() != 0))
+        {
+          // Version one is longer than number two, and non-zero
+          return 1;
+        }
+      }
+      // Version one is longer than version two, but zero
+      return 0;
+    }
     
-    if (r.isDigit())
+    QString suffix1, suffix2;
+    int number1 = this->extractReleaseNumber(ver1[i], suffix1);
+    int number2 = this->extractReleaseNumber(ver2[i], suffix2);
+    
+     if (number1 < number2) {
+          // Number one is less than number two
+          return -1;
+      }
+      if (number1 > number2) {
+          // Number one is greater than number two
+          return 1;
+      }
+
+      bool empty1 = suffix1.length() == 0;
+      bool empty2 = suffix2.length() == 0;
+
+      if (empty1 && empty2) continue; // No suffixes
+      if (empty1) return 1; // First suffix is empty (1.2 > 1.2b)
+      if (empty2) return -1; // Second suffix is empty (1.2a < 1.2)
+
+      // Lexical comparison of suffixes
+      int result = suffix1.compare(suffix2);
+      if (result != 0) return result;
+  }
+  if (i < ver2.count())
+  {
+    for (int j = i ; j < ver2.count() ; ++j)
     {
-      if (!m.isDigit())
-        return false;
-      else if (m.digitValue() < r.digitValue())
-        return false;
+      QString suffix2;
+      int number2 = this->extractReleaseNumber(ver2[j], suffix2);
+      if ((number2 != 0) || (suffix2.length() != 0))
+      {
+        // Version one is longer than version two, and non-zero
+        return -1;
+      }
     }
-    else if (!m.isDigit())
-    {
-      if (m <  r)
-        return false;
-    }
+    // Version two is longer than version one, but zero
+    return 0;
+  }
+  
+  return 0;
+}
+
+int UpdateParser::extractReleaseNumber(const QString& str, QString& suffix) const
+{
+  int num = 0;
+  int inc = 0;
+  while (inc < str.length())
+  {
+    QChar c = str.at(inc);
+    if (c.isDigit())    
+      num = num * 10 + c.digitValue();
     else
       break;
+    ++inc;
   }
-  return true;
+  suffix = str.mid(inc+1);
+  return num;
 };
 
 // Dialog
