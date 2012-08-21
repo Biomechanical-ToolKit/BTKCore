43884917c081a21ac90fea67b19ccfbe5818ca5b
arnaud.barre <arnaud.barre@93be6e1e-6fcc-11de-baec-69240fdc299a>
2012-08-21 21:52:34 +0000

    [FIX] Most of the Python unit tests were commented...
    git-svn-id: https://b-tk.googlecode.com/svn/BTK/branches/python@1195 93be6e1e-6fcc-11de-baec-69240fdc299a

--- Testing/Python/_TDDBasicFilters.py
+++ Testing/Python/_TDDBasicFilters.py
@@ -10,11 +10,11 @@ import SeparateKnownVirtualMarkersFilterTest
 
 def suite():
     suite = unittest.TestSuite()
-    # suite.addTest(unittest.makeSuite(AcquisitionUnitConverterTest.AcquisitionUnitConverterTest))
+    suite.addTest(unittest.makeSuite(AcquisitionUnitConverterTest.AcquisitionUnitConverterTest))
     suite.addTest(unittest.makeSuite(DownsampleFilterTest.DownsampleFilterTest))
-    #     suite.addTest(unittest.makeSuite(ForcePlatformsExtractorTest.ForcePlatformsExtractorTest))
-    #     suite.addTest(unittest.makeSuite(GroundReactionWrenchFilterTest.GroundReactionWrenchFilterTest))
-    #     suite.addTest(unittest.makeSuite(IMUsExtractorTest.IMUsExtractorTest))
-    #     suite.addTest(unittest.makeSuite(MergeAcquisitionFilterTest.MergeAcquisitionFilterTest))
-    #     suite.addTest(unittest.makeSuite(SeparateKnownVirtualMarkersFilterTest.SeparateKnownVirtualMarkersFilterTest))
+        suite.addTest(unittest.makeSuite(ForcePlatformsExtractorTest.ForcePlatformsExtractorTest))
+        suite.addTest(unittest.makeSuite(GroundReactionWrenchFilterTest.GroundReactionWrenchFilterTest))
+        suite.addTest(unittest.makeSuite(IMUsExtractorTest.IMUsExtractorTest))
+        suite.addTest(unittest.makeSuite(MergeAcquisitionFilterTest.MergeAcquisitionFilterTest))
+        suite.addTest(unittest.makeSuite(SeparateKnownVirtualMarkersFilterTest.SeparateKnownVirtualMarkersFilterTest))
     return suite
