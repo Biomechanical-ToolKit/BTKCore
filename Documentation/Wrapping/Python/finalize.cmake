1ca68a3a34ee5a379705dadaf74c6f3f8b06d25c
arnaud.barre <arnaud.barre@93be6e1e-6fcc-11de-baec-69240fdc299a>
2012-09-05 22:15:04 +0000

    [FIX] Doxygen configuration files updated to use Doxygen 1.8.2.
    git-svn-id: https://b-tk.googlecode.com/svn/BTK/trunk@1223 93be6e1e-6fcc-11de-baec-69240fdc299a

--- Documentation/Wrapping/Python/finalize.cmake
+++ Documentation/Wrapping/Python/finalize.cmake
@@ -31,28 +31,18 @@ FOREACH(f ${files})
      "<td class=\"paramname\"><em>self</em></td><td>)</td>"
      "<td class=\"paramname\"><em>self</em></td><td>&nbsp;</td><td>)</td>"
      OUT "${OUT}")
-  # Remove the brief description in the detailed description as the second line is the same
-  STRING(REGEX REPLACE ".*<\\/h2>
-<div class=\"textblock\">(<p>.*<\\/p>)
-<div class=\"fragment\">.*" "\\1" brief "${OUT}")
   STRING(REPLACE "${brief}" "" OUT "${OUT}")
   # Rewrite the file
   FILE(WRITE "${f}" "${OUT}")
 ENDFOREACH(f)
-# Adapt the content of the file "hierarchy.html" to remove the class btk::_object
-SET(f "${BTK_BINARY_DIR}/Documentation/Wrapping/Python/hierarchy.html")
-FILE(READ "${f}" IN)
-STRING(REPLACE "<li><b>btk._object</b><ul>" "" OUT "${IN}")
-STRING(REPLACE "</li>
-</ul>
-</div><!-- contents -->" "</div><!-- contents -->" OUT "${OUT}")
-FILE(WRITE "${f}" "${OUT}")
 
 # Adapt the content of the file "namespacebtk.html" to remove the class btk::_object and btk::SwigPyIterator
 SET(f "${BTK_BINARY_DIR}/Documentation/Wrapping/Python/namespacebtk.html")
 FILE(READ "${f}" IN)
 STRING(REPLACE "<tr class=\"memitem:\"><td class=\"memItemLeft\" align=\"right\" valign=\"top\">class &#160;</td><td class=\"memItemRight\" valign=\"bottom\"><b>_object</b></td></tr>
 <tr class=\"memdesc:\"><td class=\"mdescLeft\">&#160;</td><td class=\"mdescRight\">Abstract interface for the Python wrapping of BTK. <br/></td></tr>
-<tr class=\"memitem:\"><td class=\"memItemLeft\" align=\"right\" valign=\"top\">class &#160;</td><td class=\"memItemRight\" valign=\"bottom\"><b>SwigPyIterator</b></td></tr>"
+<tr class=\"separator:\"><td class=\"memSeparator\" colspan=\"2\">&#160;</td></tr>
+<tr class=\"memitem:\"><td class=\"memItemLeft\" align=\"right\" valign=\"top\">class &#160;</td><td class=\"memItemRight\" valign=\"bottom\"><b>SwigPyIterator</b></td></tr>
+<tr class=\"separator:\"><td class=\"memSeparator\" colspan=\"2\">&#160;</td></tr>"
 "" OUT "${IN}")
 FILE(WRITE "${f}" "${OUT}")
