Biomechanical-ToolKit/BTKCore

commit b6a6e8f3523a3882b19a37754ebb4cf643c0491e

[UPD] Update mechanism updated to use only the inputs and not their source to test if the current block has to be update. Moreover, the outputs' timestamp is no more updated, as it is possible that some of them were not modified during the update process.
git-svn-id: https://b-tk.googlecode.com/svn/BTK/trunk@1309 93be6e1e-6fcc-11de-baec-69240fdc299a

Code/Common/btkProcessObject.cpp
@@ -169,11 +169,6 @@ namespace btk
         this->m_Inputs[inc]->Update();
         if (this->m_Inputs[inc]->m_Timestamp >= this->m_Timestamp)
           this->m_Modified = true;
-        if (this->m_Inputs[inc]->mp_Source)
-        {
-          if (this->m_Inputs[inc]->mp_Source->m_Timestamp >= this->m_Timestamp)
-            this->m_Modified = true;
-        }
       }
     }
     if (this->m_Modified)
@@ -181,8 +176,6 @@ namespace btk
       this->GenerateData();
       this->Object::Modified();
       this->m_Modified = false;
-      for (std::vector<DataObject::Pointer>::iterator it = this->m_Outputs.begin() ; it != this->m_Outputs.end() ; ++it)
-        (*it)->Modified();
     }
     this->m_Updating = false;
   };

Documentation/API/AdvancedTutorial.dox
@@ -109,7 +109,6 @@ digraph processUpdate
     generate_data [ shape=rect, label="Generate output(s)\n(btk::ProcessObject::GenerateData())" ];
     modify_process2 [ shape=rect, label="Update process' timestamp\nbtk::Object::Modified()"];
     unmodify_process [ shape=rect, label="Set process tag as unmodified"];
-    modify_outputs [ shape=rect, label="Update outputs' timestamp\nbtk::Object::Modified()"];
     label = "Update process";
   }
   setUpdatingFalse [ shape=rectangle, label="Set updating flag to false" ]
@@ -129,9 +128,8 @@ digraph processUpdate
   modified_process -> generate_data [label="yes", headport = "n", tailport = "s"];
   generate_data -> modify_process2 [headport = "n", tailport = "s"];
   modify_process2 -> unmodify_process [headport = "n", tailport = "s"];
-  unmodify_process -> modify_outputs [headport = "n", tailport = "s"];
+  unmodify_process -> setUpdatingFalse [headport = "n", tailport = "s"];
   modified_process -> end [label="no" headport = "n", tailport = "e"];
-  modify_outputs -> setUpdatingFalse [headport = "n", tailport = "s"];
   setUpdatingFalse -> end [headport = "n", tailport = "s"];
   label = "Workflow of the method btk::ProcessObject::Update()"; 
 }

commit bb7c6f097accac35afd68226b352fbf3d079d5fd

[FIX] The method ProcessObject::Modified sets now only the modification flag to true. The time stamp is now only updated when the filter is updated. This fix a problem when a new input is set (introduced in revision r1309). The method ProcessObject::Update was also adapted in consequence.
git-svn-id: https://b-tk.googlecode.com/svn/BTK/trunk@1324 93be6e1e-6fcc-11de-baec-69240fdc299a

Code/Common/btkProcessObject.cpp
@@ -173,8 +173,14 @@ namespace btk
     }
     if (this->m_Modified)
     {
+      unsigned long int ts = this->GetTimestamp();
       this->GenerateData();
       this->Object::Modified();
+      for (size_t inc = 0 ; inc < this->m_Outputs.size() ; ++inc)
+      {
+        if ((this->m_Outputs[inc].get() != 0) && (this->m_Outputs[inc]->GetTimestamp() > ts))
+          this->m_Outputs[inc]->m_Timestamp = this->m_Timestamp;
+      }
       this->m_Modified = false;
     }
     this->m_Updating = false;
@@ -367,7 +373,7 @@ namespace btk
   void ProcessObject::Modified()
   {
     this->m_Modified = true;
-    this->Object::Modified();
+    // this->Object::Modified();
   };
   
   /**

Testing/Code/PipelineTest.h
@@ -133,6 +133,37 @@ CXXTEST_SUITE(PipelineTest)
     res->Update();
     TS_ASSERT_EQUALS(res->GetValue(), 6);
   };
+  
+  CXXTEST_TEST(NewInput)
+  {
+    Source::Pointer src = Source::New();
+    src->SetValue(5);
+    Filter::Pointer incFilt1 = Filter::New();
+    incFilt1->SetInput(src);
+    Source::Pointer res1 = incFilt1->GetOutput();
+    Filter::Pointer incFilt2 = Filter::New();
+    incFilt2->SetInput(incFilt1->GetOutput());
+    Source::Pointer res2 = incFilt2->GetOutput();
+    res2->Update();
+    Source::Pointer src2 = Source::New();
+    incFilt1->SetInput(src2);
+    res2->Update();
+    TS_ASSERT_EQUALS(res1->GetValue(), 1);
+    TS_ASSERT_EQUALS(res2->GetValue(), 2);
+    Source::Pointer src3 = Source::New();
+    src3->SetValue(5);
+    incFilt1->SetInput(src3);
+    res2->Update();
+    TS_ASSERT_EQUALS(res1->GetValue(), 6);
+    TS_ASSERT_EQUALS(res2->GetValue(), 7);
+    Source::Pointer src4 = Source::New();
+    src4->SetValue(10);
+    incFilt1->SetInput(src4);
+    incFilt2->SetInc(2);
+    res2->Update();
+    TS_ASSERT_EQUALS(res1->GetValue(), 11);
+    TS_ASSERT_EQUALS(res2->GetValue(), 13);
+  };
 };
 
 CXXTEST_SUITE_REGISTRATION(PipelineTest)
@@ -140,4 +171,5 @@ CXXTEST_TEST_REGISTRATION(PipelineTest, PipelineOne)
 CXXTEST_TEST_REGISTRATION(PipelineTest, PipelineTwo)
 CXXTEST_TEST_REGISTRATION(PipelineTest, PipelineThree)
 CXXTEST_TEST_REGISTRATION(PipelineTest, DeleteParent)
+CXXTEST_TEST_REGISTRATION(PipelineTest, NewInput)
 #endif

Testing/Code/PointCollectionTest.h
@@ -3,6 +3,13 @@
 
 #include <btkPointCollection.h>
 
+#ifdef _MSC_VER
+  #ifndef WIN32_LEAN_AND_MEAN
+    #define WIN32_LEAN_AND_MEAN
+  #endif
+  #include <windows.h>
+#endif
+
 CXXTEST_SUITE(PointCollectionTest)
 {
   CXXTEST_TEST(Constructor)
@@ -24,9 +31,13 @@ CXXTEST_SUITE(PointCollectionTest)
   CXXTEST_TEST(ClearModified)
   {
     btk::PointCollection::Pointer test = btk::PointCollection::New();
-    btk::Point::Pointer m = btk::Point::New("HEEL_R", 10);
-    test->InsertItem(0, m);
+    test->InsertItem(0, btk::Point::New("HEEL_R", 10));
     unsigned long int t1 = test->GetTimestamp();
+#ifndef _MSC_VER
+    sleep(1); // Wait 1 msec ... Because the clear action can take less than 1 usec...
+#else
+    ::Sleep(1);
+#endif
     test->Clear();
     TS_ASSERT(test->GetTimestamp() != t1);
   };