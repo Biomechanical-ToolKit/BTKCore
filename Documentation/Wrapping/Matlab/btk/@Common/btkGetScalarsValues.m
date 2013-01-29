d8f9a47b319551e2ae316b67cc127f1911f69e96
arnaud.barre <arnaud.barre@93be6e1e-6fcc-11de-baec-69240fdc299a>
2013-01-29 00:13:07 +0000

    [FIX] Double space typo.
    git-svn-id: https://b-tk.googlecode.com/svn/BTK/trunk@1527 93be6e1e-6fcc-11de-baec-69240fdc299a

--- Documentation/Wrapping/Matlab/btk/@Common/btkGetScalarsValues.m
+++ Documentation/Wrapping/Matlab/btk/@Common/btkGetScalarsValues.m
@@ -4,7 +4,7 @@ function v = btkGetScalarsValues(h) %#ok
 %  V = BTKGETSCALARSVALUES(H) returns a matrix of real.  The number of rows
 %  of this matrix corresponds to the number of frames in the acquisition.
 %  The number of columns of this matrix corresponds to the number of 
-% extracted scalars multiplied by 3 (their components X,Y,Z).
+%  extracted scalars multiplied by 3 (their components X,Y,Z).
 %  The biomechanical acquisition handle H is represented as a double
 %  and can be only used with the btk* functions.
 
