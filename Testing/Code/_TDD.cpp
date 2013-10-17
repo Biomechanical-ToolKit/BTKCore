c7ff2da3b45de66a7d4af0e5f0d173adfff221e8
Arnaud Barré <arnaud.barre@gmail.com>
2013-10-17 13:32:35 -0400

    [UPD] Use of the command btk::Logger::SetVerbose instead of setting the stream buffer of std::cerr to null when the symbol TDD_SILENT_CERR is defined.

--- Testing/Code/_TDD.cpp
+++ Testing/Code/_TDD.cpp
@@ -1,21 +1,16 @@
 #include "_TDDConfigure.h"
 
+#include <btkLogger.h>
+
 // BTK error messages are not displayed
 #define TDD_SILENT_CERR
 
 int main()
 {
 #if defined(TDD_SILENT_CERR)
-  std::streambuf* standardErrorOutput = std::cerr.rdbuf(0);
+  btk::Logger::SetVerboseMode(btk::Logger::Quiet);
 #endif
-
-  int err = CxxTest::ErrorPrinter().run();
-  
-#if defined(TDD_SILENT_CERR)
-  std::cerr.rdbuf(standardErrorOutput);
-#endif
-
-  return err;
+  return CxxTest::ErrorPrinter().run();;
 };
 
 #include <cxxtest/Root.cpp>
