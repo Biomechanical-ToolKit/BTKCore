06982f8ac2a2ed56d4bed3a2b741073810501795
Arnaud Barré <arnaud.barre@gmail.com>
2013-12-28 11:15:31 -0500

    [UPD] New Matlab function btkSetMetaDataValues.

---
+++ Documentation/Wrapping/Matlab/btk/btkSetMetaDataValues.m
@@ -0,0 +1,14 @@
+function md = btkSetMetaDataValues(h, varargin) %#ok
+%BTKSETMETADATAVALUES  Set values of an existing metada.
+%
+%  BTKSETMETADATAVALUES is only an alias for the function <a href="matlab:help btkSetMetaData">btkSetMetaData</a>.
+%
+%  The acquisition is represented by the handle H.  This handle is obtained
+%  by the use of a btk* function.
+
+%  Author: A. Barré
+%  Copyright 2009-2013 Biomechanical ToolKit (BTK).
+
+md = btkSetMetaData(h, varargin{:});
+
+% [EOF] btkSetMetaDataValues.m
