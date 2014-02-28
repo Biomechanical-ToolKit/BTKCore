f2e602d460c790833cfd1bcfc9e22f21fac7fc91
Arnaud Barré <arnaud.barre@gmail.com>
2014-02-28 10:42:20 -0500

    [UPD] FindMatlab updated to detected Matlab r2013(a|b) versions.

--- CMake/FindMatlab.cmake
+++ CMake/FindMatlab.cmake
@@ -3,7 +3,7 @@
 #   a macro to help the build of MEX-functions.
 #
 # This module detects a Matlab's version between Matlab 7.0 (R14)
-# and Matlab 8.0 (r2012b).
+# and Matlab 8.2 (r2013b).
 #
 # This module defines: 
 #  MATLAB_ROOT:               Matlab installation path
@@ -34,6 +34,8 @@ ENDIF(MATLAB_ROOT AND MATLAB_EXECUTABLE AND MATLAB_INCLUDE_DIR AND MATLAB_LIBRAR
 IF(WIN32)
   # Default paths
   SET(MATLAB_PATHS 
+    "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MathWorks\\MATLAB\\8.2;MATLABROOT]"
+    "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MathWorks\\MATLAB\\8.1;MATLABROOT]"
     "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MathWorks\\MATLAB\\8.0;MATLABROOT]"
     "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MathWorks\\MATLAB\\7.15;MATLABROOT]"
     "[HKEY_LOCAL_MACHINE\\SOFTWARE\\MathWorks\\MATLAB\\7.14;MATLABROOT]"
@@ -55,6 +57,8 @@ IF(WIN32)
   # Paths for Matlab 32-bit under Windows 64-bit
   IF(NOT MATLAB_ROOT)
     SET(MATLAB_PATHS 
+      "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MathWorks\\MATLAB\\8.2;MATLABROOT]"
+      "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MathWorks\\MATLAB\\8.1;MATLABROOT]"
       "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MathWorks\\MATLAB\\8.0;MATLABROOT]"
       "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MathWorks\\MATLAB\\7.15;MATLABROOT]"
       "[HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\MathWorks\\MATLAB\\7.14;MATLABROOT]"
