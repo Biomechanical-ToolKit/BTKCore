97cc3c46dbbd3ec5f7035571edf5a5c86982d298
Arnaud Barré <arnaud.barre@gmail.com>
2015-01-05 19:21:16 -0500

    Documentation improved for the class btk::Object.

--- Code/Common/btkObject.cpp
+++ Code/Common/btkObject.cpp
@@ -61,6 +61,176 @@ namespace btk
    * @class Object btkObject.h
    * @brief Base for all objects which need to keep track of modified time.
    *
+   * This class uses the private implementation (pimpl) idiom to keep an ABI compatibility
+   * between the different release of the project.
+   *
+   * Two macros are provided to simplify the implementation of the pimpl idiom.
+   * Both of them are defined in the file "btkOpaque.h".
+   *  - BTK_DECLARE_PIMPL_ACCESSOR: Creates pimpl() methods in the public interface to access to the private part (opaque pointer)
+   *  - BTK_DECLARE_PINT_ACCESSOR: Creates pint() methods in the private implementation to access to the public part
+   *
+   * For example, the following header file create a new class which contains an accessor and mutator for a member 'Version' in the (opaque) private implementation.
+   * The macro BTK_DECLARE_PIMPL_ACCESSOR declares private pimpl() methods used to access to the mp_Pimpl member.
+   * The methods are able to downcast to the given classname. This is usefull when the implemented class inherits from a class which uses already the pimpl idiom.
+   *
+   * To use the macros for custom development you can follow the next examples. The first and second introduce the macros while the third introduce their use to inherit from the class btk::Object
+   *
+   * First the public interface is defined in a header file.
+   *
+   * @code
+   * class MyObject
+   * {
+   *   BTK_DECLARE_PIMPL_ACCESSOR(MyObject) // pimpl() methods
+   *   
+   * public:
+   *   MyObject();
+   *   int version() const noexcept;
+   *   void setVersion() const noexcept:
+   *
+   *   void print() const;
+   *  
+   * private:
+   *   struct Private; // Forward declaration
+   *   std::unique_ptr<Private> mp_Pimpl; // RAII storage
+   * };
+   * @endcode 
+   *
+   * The associated implementation file can be presented as in the following example. The MyObject::Private data structure is defined as well as the needed methods for the class MyObject.
+   *
+   * @code
+   * struct MyObject::Private
+   * {
+   *   Private();
+   *   int Version;
+   * };
+   *
+   * MyObject::Private::Private()
+   * : Version(1)
+   * {};
+   *
+   * MyObject::MyObject()
+   * : mp_Pimpl(new Private(this))
+   * {};
+   * 
+   * int MyObject::version() const
+   * {
+   *   auto optr = this->pimpl();
+   *   return optr->Version;
+   * };
+   * 
+   * void MyObject::setVersion(int value)
+   * {
+   *   auto optr = this->pimpl();
+   *   optr->Version = value;
+   * };
+   *
+   * void MyObject::print() const
+   * {
+   *   std::cout << this->version() << std::endl;
+   * };
+   * @endcode
+   *
+   * In the following example, the macro defining the accessors of the public interface in the private implementation is used. It is also important to declare a member @a mp_Pint and a constructor with a pointer to the public interface to be useable.
+   *
+   * @code
+   * struct MyObject::Private
+   * {
+   *   BTK_DECLARE_PINT_ACCESSOR(MyObject) // pint() methods
+   *   Private(MyObject* pint); // Constructor with a pointer associated to the public interface object
+   *   MyObject* mp_Pint;
+   *   int Version;
+   *   void foo(); // internal method
+   * };
+   *
+   * MyObject::Private::Private(MyObject* pint)
+   * : mp_Pint(pint), Version(1)
+   * {};
+   * 
+   * MyObject::Private::foo()
+   * {
+   *   this->pint()->print(); // Call a method of the public interface.
+   * }
+   *
+   * MyObject::MyObject()
+   * : mp_Pimpl(new Private(this))
+   * {};
+   * 
+   * int MyObject::version() const
+   * {
+   *   auto optr = this->pimpl();
+   *   return optr->Version;
+   * };
+   * 
+   * void MyObject::setVersion(int value)
+   * {
+   *   auto optr = this->pimpl();
+   *   optr->Version = value;
+   * };
+   *
+   * void MyObject::print() const
+   * {
+   *   std::cout << this->version() << std::endl;
+   * };
+   * @endcode
+   *
+   * Finally, to extand BTK with a new class, you can inherit from the class btk::Object (or other inherited classes). Thus it is even simpler to use the pimpl idiom. Indeed, the member mp_Pimpl is already declared in the class btk::Object.
+   * You only need to inherit from the public and private part. Note, to inherit from the private part, this one is defined in its own private header file (with the suffix _p) to not have it visible and possible break the ABI compatibilty.
+   *
+   * @code
+   * class MyObjectPrivate; // Defined outside in case it can be reused
+   *
+   * class MyObject : public Object
+   * {
+   *   BTK_DECLARE_PIMPL_ACCESSOR(MyObject)
+   *   
+   * public:
+   *   MyObject();
+   *   int version() const noexcept;
+   *   void setVersion() const noexcept:
+   *
+   * protected:
+   *   MyObject(MyObjectPrivate& pimpl) noexcept; // Only needed if this class could be inherited
+   * };
+   * @endcode
+  
+   * The private header file (e.g. MyObject_p.h) could be like this:
+   *
+   * @code
+   * struct MyObjectPrivate : public ObjectPrivate
+   * {
+   *   MyObjectPrivate()
+   *   int Version;
+   * };
+   * @endcode
+   *
+   * And the implementation file:
+   *
+   * @code
+   * MyObject::MyObject()
+   * : btk::Object(*new MyObjectPrivate())
+   * {};
+   * 
+   * int MyObject::version() const
+   * {
+   *   auto optr = this->pimpl();
+   *   return optr->Version;
+   * };
+   * 
+   * void MyObject::setVersion(int val)
+   * {
+   *   auto optr = this->pimpl();
+   *   if (optr->Version != val)
+   *   {
+   *     optr->Version = val;
+   *     this->modified();
+   *   }
+   * };
+   *
+   * MyObject::MyObject(MyObjectPrivate& pimpl) noexcept
+   * : Object(pimpl)
+   * {};
+   * @endcode
+   *
    * @ingroup BTKCommon
    */
   
