86408e05ea3992ca3c6aa512c1cffa30c59ee1cb
Arnaud Barré <arnaud.barre@gmail.com>
2015-01-19 20:37:20 -0500

    New interface class used to read/write data from/to a Input/Output (I/O) device.

---
+++ Code/IO/btkIODevice.cpp
@@ -0,0 +1,254 @@
+/* 
+ * The Biomechanical ToolKit
+ * Copyright (c) 2009-2014, Arnaud Barré
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *     * Neither the name(s) of the copyright holders nor the names
+ *       of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written
+ *       permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "btkIODevice.h"
+#include "btkIODevice_p.h"
+
+// -------------------------------------------------------------------------- //
+//                                 PRIVATE API                                //
+// -------------------------------------------------------------------------- //
+
+namespace btk
+{
+  IODevicePrivate::IODevicePrivate()
+  : State(IODevice::GoodBit), Exception(IODevice::GoodBit)
+  {};
+  
+  IODevicePrivate::~IODevicePrivate() = default; // Cannot be inlined
+}
+
+// -------------------------------------------------------------------------- //
+//                                 PUBLIC API                                 //
+// -------------------------------------------------------------------------- //
+
+namespace btk
+{
+  /**
+   * @class IODevice btkIODevice.h
+   * @brief Interface to read and write on any type of device (e.g. File, Buffer, etc.).
+   *
+   * There is two types of devices. The first one is called "random access" (e.g. File, Buffer), while the second is called "sequential" (e.g. SerialPort).
+   * The use of a IODevice class should be used with a BinaryStream or TextStream which interact at a higher level to read/write data in a device.
+   *
+   * This interface proposes also an exception mechanism in case the device fails to do some internal operation, or if there are error during I/O operations.
+   * You can use the method setExceptions() to set the states which can trigger an exception. The exception thrown by this class corresponds to a IODevice::Failure exception.
+   *
+   * To implement a new device, several methods must me implemented. To facilitate this implementation some protected methods are proposed like verifyOpenMode().
+   */
+  
+  /**
+   * Destructor. 
+   * This methods does nothing. It is the responsability of the inherited class to decide if their destructor does something specific regarding the state of the device (like closing it).
+   */
+  IODevice::~IODevice() noexcept
+  {};
+  
+  /**
+   * Returns true if the device's state is set to GoodBit.
+   */
+  bool IODevice::isGood() const noexcept
+  {
+    auto optr = this->pimpl();
+    return optr->State == GoodBit;
+  };
+  
+  /**
+   * Returns true if the device's state contain the flag EndBit.
+   */
+  bool IODevice::isEnd() const noexcept
+  {
+    auto optr = this->pimpl();
+    return (this->state() & EndBit) != 0;
+  };
+ 
+  /**
+   * Returns true if the device's state contain the flag FailBit and/or ErrorBit.
+   */
+  bool IODevice::hasFailure() const noexcept
+  {
+    auto optr = this->pimpl();
+    return (optr->State & (FailBit | ErrorBit)) != 0;
+  };
+ 
+  /**
+   * Returns true if the device's state contain the flag FailBit
+   */
+  bool IODevice::hasError() const noexcept
+  {
+    auto optr = this->pimpl();
+    return (optr->State & ErrorBit) != 0;
+  };
+ 
+  /**
+   * Returns true current state of the device.
+   */
+  IODevice::State IODevice::state() const noexcept
+  {
+    auto optr = this->pimpl();
+    return optr->State;
+  };
+ 
+  /**
+   * Sets the state of the device.
+   * The given @a state is added to the current state.
+   * Use the method Clear() if you want to reset the state of the device.
+   */
+  void IODevice::setState(IODevice::State state)
+  {
+    this->clear(this->state() | state);
+  };
+ 
+  /**
+   * Sets the state of the device.
+   * If the given state @a flags meets some part of the exception's mask, then a Failure exception is thrown.
+   * @note Setting the state to GoodBit will reset the possible current failure/errors.
+   */
+  void IODevice::clear(IODevice::State state)
+  {
+    auto optr = this->pimpl();
+    optr->State = state;
+    if ((this->exceptions() & this->state()) != 0)
+      throw Failure("IODevice::clear");
+  };
+   
+  /**
+   * Returns the mask used to possibly throws an exception.
+   */
+  IODevice::State IODevice::exceptions() noexcept
+  {
+    auto optr = this->pimpl();
+    return optr->Exception;
+  };
+ 
+  /**
+   * Sets the mask which will be used to throw an exceptions.
+   * Setting the mask to GoodBit will cancel the use of exception with the device. 
+   */
+  void IODevice::setExceptions(IODevice::State mask)
+  {
+    auto optr = this->pimpl();
+    optr->Exception = mask;
+    this->clear(optr->State);
+  };
+  
+  /**
+   * @fn virtual bool IODevice::isOpen() = 0
+   * Returns true if the device is opened otherwise returns false.
+   */
+  
+  /**
+   * @fn virtual void IODevice::close() = 0
+   * Close the device.
+   * @note The inherited class should set the FailBit state flag if any failure happens during the closing of the device.
+   */
+  
+  /**
+   * @fn virtual void IODevice::read(char* s, Size n) = 0
+   * Gets from the device a sequence of characters of size @a n and store it in @a s. The ErrorBit state flag is set if any issue happens during the reading operation. 
+   */
+    
+  /**
+   * @fn virtual void IODevice::write(const char* s, Size n) = 0
+   * Puts the sequence of characters @a s of size @a n to the device. The ErrorBit state flag is set if any issue happens during the writing operation. 
+   */
+    
+  /**
+   * @fn virtual void IODevice::seek(Offset offset, SeekDir dir) = 0
+   * Moves the pointer associated with a random access device of the given @a offset in the given direction @a dir.
+   * @note The use of this method with a sequential device would set the flag FailBit to true.
+   */
+  
+  /**
+   * @fn virtual Position IODevice::tell() const = 0
+   * Returns the position of the pointer associated with a random access device. 
+   * @note The use of this method with a sequential device would set the flag FailBit to true.
+   */
+  
+  /**
+   * @fn virtual bool IODevice::isSequential() const = 0
+   * Returns true if the device is sequential otherwise false.
+   */
+  
+  /**
+   * Verify the coherency of the given open mode.
+   * This method should be used by every inherited class in their Open() method before trying to open the implemented device.
+   * Internally, this will set the flag FailBit to true if one of the verification is not valid.
+   * The following checks are realized by this method:
+   *  - The device is not already opened.
+   *  - The modes Append and Truncate cannot be set at the same time.
+   *  - The mode Truncate is set but not the mode Out.
+   */
+  bool IODevice::verifyOpenMode(OpenMode mode)
+  {
+    bool valid = true;
+    if (this->isOpen()
+      || (((mode & Append) == Append) && ((mode & Truncate) == Truncate))
+      || (((mode & Truncate) == Truncate) && ((mode & Out) != Out)))
+    {
+      valid = false;
+      this->setState(FailBit);
+    }
+    return valid;
+  };
+  
+  /**
+   * Constructor.
+   * Set the device's state to GoodBit without exception enabled.
+   */
+  IODevice::IODevice() noexcept
+  : mp_Pimpl(new IODevicePrivate)
+  {};
+  
+  IODevice::IODevice(IODevicePrivate& pimpl) noexcept
+  : mp_Pimpl(&pimpl)
+  {};
+  
+  // ----------------------------------------------------------------------- //
+  
+  /**
+   * @class IODevice::Failure btkIODevice.h
+   * @brief Exception thrown by the inherited classes of the interface IODevice.
+   */
+  
+  /**
+   * @fn IODevice::Failure::Failure(const std::string& msg);
+   * Constructor.
+   */
+  
+  /**
+   * @fn virtual IODevice::Failure::~Failure()
+   * Destructor (empty).
+   */
+};
---
+++ Code/IO/btkIODevice.h
@@ -0,0 +1,125 @@
+/* 
+ * The Biomechanical ToolKit
+ * Copyright (c) 2009-2014, Arnaud Barré
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ *     * Redistributions of source code must retain the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *     * Neither the name(s) of the copyright holders nor the names
+ *       of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written
+ *       permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef __btkIODevice_h
+#define __btkIODevice_h
+
+#include "btkIOExport.h"
+#include "btkOpaque.h"
+#include "btkException.h"
+
+#include <memory> // unique_ptr
+#include <ios>
+
+namespace btk
+{
+  class IODevicePrivate;
+  
+  class BTK_IO_EXPORT IODevice
+  {
+    BTK_DECLARE_PIMPL_ACCESSOR(IODevice)
+    
+  public:
+    typedef std::ios_base::iostate State;
+    static const State EndBit = std::ios_base::eofbit;
+    static const State FailBit = std::ios_base::failbit;
+    static const State ErrorBit = std::ios_base::badbit;
+    static const State GoodBit = std::ios_base::goodbit;
+
+    typedef std::ios_base::seekdir SeekDir;
+    static const SeekDir Begin = std::ios_base::beg;
+    static const SeekDir Current = std::ios_base::cur;
+    static const SeekDir End = std::ios_base::end;
+    
+    typedef std::ios_base::openmode OpenMode;
+    static const OpenMode In = std::ios_base::in;
+    static const OpenMode Out = std::ios_base::out;
+    static const OpenMode Append = std::ios_base::app;
+    static const OpenMode Truncate = std::ios_base::trunc;
+    static const OpenMode AtEnd = std::ios_base::ate;
+    
+    typedef std::streamsize Size;
+    typedef std::streamoff Offset;
+    typedef std::streampos Position;
+    
+    class Failure : public Exception
+    {
+    public:
+     explicit Failure(const std::string& msg) noexcept : Exception(msg) {};
+     virtual ~Failure() noexcept = default;
+    };
+    
+    IODevice(const IODevice& ) = delete;
+    IODevice(IODevice&& ) noexcept = delete;
+    IODevice& operator=(const IODevice& ) = delete;
+    IODevice& operator=(IODevice&& ) noexcept = delete;
+
+    virtual ~IODevice() noexcept;
+    
+    // State
+    bool isGood() const noexcept;
+    bool isEnd() const noexcept;
+    bool hasFailure() const noexcept;
+    bool hasError() const noexcept;
+    State state() const noexcept;
+    void setState(State state);
+    void clear(State state = GoodBit);
+    State exceptions() noexcept;
+    void setExceptions(State mask);
+    
+    // General
+    virtual bool isOpen() const noexcept = 0;
+    virtual void close() = 0;
+    virtual void read(char* s, Size n) = 0;
+    virtual void write(const char* s, Size n) = 0;
+    
+    // Random access IO device only
+    virtual void seek(Offset offset, SeekDir dir) = 0;
+    virtual Position tell() const noexcept = 0;
+    
+    // Sequential IO only
+    virtual bool isSequential() const noexcept = 0;
+    
+  protected:
+    virtual bool verifyOpenMode(OpenMode mode);
+    
+    IODevice() noexcept;
+    IODevice(IODevicePrivate& pimpl) noexcept;
+   
+    std::unique_ptr<IODevicePrivate> mp_Pimpl;
+  };
+};
+
+#endif // __btkIODevice_h
