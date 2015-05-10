baec5f8100973d5ec8808bb4fc8c0e1d77de1194
Arnaud Barré <arnaud.barre@gmail.com>
2015-05-10 11:19:39 -0400

    Removing of the folder Batch. This folder might be reintroduced if necessary.

--- Batch/EasyInstallWithRedistributableScilabToolbox_MSVC.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-:: Global options used into EasyInstall_MSVC
-:: Limited to 5 options
-SET options.1=-DBTK_WRAP_SCILAB:BOOL=ON
-SET options.2=-DBTK_WRAP_SCILAB_REDISTRIBUTABLE_MEX_FILES:BOOL=ON
-CALL .\EasyInstall_MSVC.bat build-Scilab-Release-Redistribuable %1 %2
