4393379006b9157e85f3dc5854c60ee049b930fb
Arnaud Barré <arnaud.barre@gmail.com>
2015-05-10 14:44:33 -0400

    All the Packaging folder is removed.

--- Packaging/Matlab/btkPackageMatlabBinding.cmake
+++
@@ -1,5 +0,0 @@
-MESSAGE(STATUS "---------------------------")
-MESSAGE(STATUS "Packaging Matlab Binding...")
-MESSAGE(STATUS "---------------------------")
-
-MESSAGE(STATUS "WARNING: The packaging of the Matlab binding is not yet implemented!")
--- Packaging/Octave/btkPackageOctaveBinding.cmake
+++
@@ -1,5 +0,0 @@
-MESSAGE(STATUS "---------------------------")
-MESSAGE(STATUS "Packaging Octave Binding...")
-MESSAGE(STATUS "---------------------------")
-
-MESSAGE(STATUS "WARNING: The packaging of the Octave binding is not yet implemented!")
