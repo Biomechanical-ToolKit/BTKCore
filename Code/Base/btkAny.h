Biomechanical-ToolKit/BTKCore

commit 6c895726e4e59391c1746444568569399fc7f1b6

Headers moved.

Code/Base/btkAny.h
@@ -37,8 +37,6 @@
 #define __btkAny_h
 
 #include "btkBaseExport.h"
-#include "btkTypeTraits.h"
-#include "btkTypeid.h"
 
 #include <type_traits>
 #include <vector>

Code/Base/btkAny.tpp
@@ -37,6 +37,7 @@
 #define __btkAny_tpp
 
 #include "btkException.h"
+#include "btkTypeid.h"
 #include "btkTypeTraits.h"
 #include "btkMacros.h" // _BTK_CONSTEXPR
 

commit 963e24f0b3460b6bb2c51a998e4f90a974aa3b11

Commit 6c89572 introduced a GCC compiler error (Missing size_t type).

Code/Base/btkAny.h
@@ -42,6 +42,7 @@
 #include <type_traits>
 #include <vector>
 #include <initializer_list>
+#include <cstddef> // size_t
 
 namespace btk
 {